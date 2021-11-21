6d26cb4c0c368414d34b3096d69acac743fb8653
aAnish07 <ch20btech11006@iith.ac.in>
2021-11-21 21:50:37 +0530

    allery repositioned | team section: phone hover effect active | footer revamped and remade completely(frontend) with responsiveness

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+const buttonOne = document.querySelector(".collapse.one");
+
+buttonOne.onclick = () => {
+  document.querySelector(".collapsible.one").classList.toggle("expanded");
+  buttonOne.classList.toggle("toggled");
+}
+
+const buttonTwo = document.querySelector(".collapse.two");
+
+buttonTwo.onclick = () => {
+  document.querySelector(".collapsible.two").classList.toggle("expanded");
+  buttonTwo.classList.toggle("toggled");
+}
+
