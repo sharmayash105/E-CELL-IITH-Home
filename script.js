f3f2ea3b419ebd1e5a75aeba2dd4159276479d12
Anish Ambavat <ch20btech11006@iith.ac.in>
2021-11-27 02:56:46 +0530

    footer modified | added transitions | cleaned up

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+  navbarLinks.classList.toggle('expanded')
 })
 
 const buttonOne = document.querySelector(".collapse.one");
